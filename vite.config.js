f581bd9eab4a5c3485b05dec8a53e823b85117d9
Imran S <trade2cart@gmail.com>
2025-11-15 23:33:44 +0530

    robot

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,13 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import sitemapPlugin from 'vite-plugin-sitemap'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react(), sitemapPlugin({
-    robots: false
-  })
-, sitemapPlugin({ hostname: 'https://vendor.trade2cart.in' })],
+  plugins: [
+    tailwindcss(),
+    react(),
+    sitemapPlugin({
+      hostname: 'https://vendor.trade2cart.in',
+      robots: false
+    })
+  ],
 })
